c7a7b2a2f3473c217274dbb4ffa250764694b5f8
Muhammed Basheer <basheerkomassery@gmail.com>
2022-09-20 23:17:16 +0530

    added command or keyword argument get_response.status_code to get status code printed

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,9 +1,10 @@
 from urllib import request
 import requests
 
-endpoint = "http://127.0.0.1:8000/api/"
+endpoint = "http://localhost:8000/api/"
 #endpoint = "https://httpbin.org/anything"
 
 get_response = requests.get(endpoint)
 print(get_response.text)
+print(get_response.status_code)
 print(get_response.json()['message'])
