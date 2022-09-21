7e8208b0f9bc7f70ae7382cf3d3379bf7c8b0150
Muhammed Basheer <basheerkomassery@gmail.com>
2022-09-22 00:34:43 +0530

    simplified conversion of django model into python dictionary by using model_to_dict function available in django.forms.models and Responded to api request with jsonresponse fuction to convert python dictionary to json data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,19 @@
-import json
-from django.http import JsonResponse
+#import json
+from django.http import JsonResponse, HttpResponse
 from products.models import Product
+from django.forms.models import model_to_dict
 
 def api_home(request,*args,**kwargs):
     body = request.body
     data = {}
     products = Product.objects.all().order_by("?").first()
     if products:
-        data['title'] = products.title
-        data['contents'] = products.contents
-        data['price'] = products.price
+        data = model_to_dict(products, fields=['id','title','price']) 
+        # model_to_dict function allows us to convert a django model in to python dictionary and we can
+        # send that model as json data by using JsonResponse fuction
+        #data['title'] = products.title
+        #data['contents'] = products.contents
+        #data['price'] = products.price
     else:
         data['title'] = "no data found"
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,5 +5,7 @@ endpoint = "http://localhost:8000/api/"
 #endpoint = "https://httpbin.org/anything"
 
 get_response = requests.get(endpoint,params={"white":"black"},json={"query":"hello world"})
-print(get_response.status_code)
+#print(get_response.status_code)
 print(get_response.json())
+#print(get_response.text)
+#print(get_response.headers)
