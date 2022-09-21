e7bd4dfb1f47eb4fe7e584f1b8e40c897c11d853
Muhammed Basheer <basheerkomassery@gmail.com>
2022-09-21 22:24:50 +0530

    created a data base model, accessed it in django application and send it to client by API response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,25 @@
 import json
 from django.http import JsonResponse
+from products.models import Product
 
 def api_home(request,*args,**kwargs):
     body = request.body
     data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    data['headers'] = dict(request.headers) # use python dict function to convert headers (json object) to python dictionary.
-    data['content_type'] = request.content_type
-    data['params'] = dict(request.GET)
-    print(data.keys())
-    print(body)
-    print(request.GET) # request.GET will help us to access params we passed from client through url as Query dict.
+    products = Product.objects.all().order_by("?").first()
+    if products:
+        data['title'] = products.title
+        data['contents'] = products.contents
+        data['price'] = products.price
+    else:
+        data['title'] = "no data found"
+
     return JsonResponse(data)
+    #    data = json.loads(body)
+    #except:
+    #data['headers'] = dict(request.headers) # use python dict function to convert headers (json object) to python dictionary.
+    #data['content_type'] = request.content_type
+    #data['params'] = dict(request.GET)
+    #print(data.keys())
+    #print(body)
+    #print(request.GET) # request.GET will help us to access params we passed from client through url as Query dict.
+    #return JsonResponse(data)
