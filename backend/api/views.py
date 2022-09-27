5a4a570f302b39e77aa2e9079f72a0b2237ea20c
Muhammed Basheer <basheerkomassery@gmail.com>
2022-09-27 08:19:15 +0530

    used rest_framework serializer function for converting data into python data type, instead of using model_to_dict function

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,29 +1,25 @@
 #import json
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse
 from products.models import Product
 from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from products.serializers import ProductSerializer
 
+@api_view(["GET"])
 def api_home(request,*args,**kwargs):
-    body = request.body
+    """
+    DRF API VIEW
+    """
+    #body = request.body
     data = {}
-    products = Product.objects.all().order_by("?").first()
-    if products:
-        data = model_to_dict(products, fields=['id','title','price']) 
-        # model_to_dict function allows us to convert a django model in to python dictionary and we can
-        # send that model as json data by using JsonResponse fuction
-        #data['title'] = products.title
-        #data['contents'] = products.contents
-        #data['price'] = products.price
+    instance = Product.objects.all().order_by("?").first()
+    if instance:
+        #data = model_to_dict(instance, fields=['id','title','price','sale_price','get_discount']) 
+        data = ProductSerializer(instance).data
+       
     else:
         data['title'] = "no data found"
 
-    return HttpResponse(data)
-    #    data = json.loads(body)
-    #except:
-    #data['headers'] = dict(request.headers) # use python dict function to convert headers (json object) to python dictionary.
-    #data['content_type'] = request.content_type
-    #data['params'] = dict(request.GET)
-    #print(data.keys())
-    #print(body)
-    #print(request.GET) # request.GET will help us to access params we passed from client through url as Query dict.
-    #return JsonResponse(data)
+    return Response(data)
+  
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,8 +4,8 @@ import requests
 endpoint = "http://localhost:8000/api/"
 #endpoint = "https://httpbin.org/anything"
 
-get_response = requests.get(endpoint,params={"white":"black"},json={"query":"hello world"})
-#print(get_response.status_code)
-#print(get_response.json())
-print(get_response.text)
-print(get_response.headers)
+get_response = requests.get(endpoint,params={"white":"black"},json={"product_id": 123 })
+print(get_response.status_code)
+print(get_response.json())
+#print(get_response.text)
+#print(get_response.headers)
